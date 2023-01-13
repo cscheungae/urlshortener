7ed7bcf01743c7e2da6426c62bbd821c9cd9eb61
Billy <cscheungae@connect.ust.hk>
2023-01-13 18:48:15 +0800

    validate url

--- src/router.ts
+++ src/router.ts
@@ -6,6 +6,13 @@ import { urlLookup, validateReqBody } from "./helper";
 const router = Router();
 
 router.get("/shorturl/:id", getUrl);
-router.post("/shorturl", body("url").isURL().custom(urlLookup), validateReqBody, uploadUrl);
+router.post(
+    "/shorturl",
+    body("url")
+        .isURL({ protocols: ["http", "https"] })
+        .custom(urlLookup),
+    validateReqBody,
+    uploadUrl
+);
 
 export default router;
